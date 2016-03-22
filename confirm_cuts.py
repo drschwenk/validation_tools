3913ca24106e33fcd4bccf4a4a3ed2b930496824
drschwenk <schwgit2@gmail.com>
2016-03-21 17:39:25 -0700

    Working interface
    Most of the structure is in place. I need to add tuple storage/ basic
    checks.

--- confirm_cuts.py
+++ confirm_cuts.py
@@ -3,6 +3,25 @@ import numpy as np
 from collections import defaultdict
 import glob
 import cv2
+import csv
+
+
+def get_key(image):
+    valid_key_presses = {32: 'pass', 13: 'mark'}
+    while True:
+        user_key = cv2.waitKey(0)
+        if user_key in valid_key_presses:
+            pass
+        elif user_key == 27:
+            new_image = cv2.putText(image, 'quit', (30, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), thickness=2)
+            cv2.imshow('movie frame', new_image)
+            cv2.waitKey(800)
+            exit()
+        else:
+            new_image = cv2.putText(image, 'try again', (30, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), thickness=2)
+            cv2.imshow('movie frame', new_image)
+            continue
+        return valid_key_presses[user_key]
 
 
 def confirm(video_name):
@@ -10,30 +29,31 @@ def confirm(video_name):
         f.write(video_name + ' is good')
 
 
-def get_user_key(image, windowName, imageClasses):
-    while True:
-        user_key = cv2.waitKey(0)
-        if(user_key == 'a'):
-            pass
-        return
-
-
-def show_image(image_name):
+def show_image(image_name, frame_n):
     drawn_image = cv2.imread(image_name, 0)
-    drawn_image = cv2.putText(drawn_image, 'frame ' + image_name.split('/')[-1],
+    drawn_image = cv2.putText(drawn_image, 'frame ' + frame_n,
                               (15, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), thickness=2)
     cv2.imshow('movie frame', drawn_image)
-    cv2.waitKey(0)
+    return get_key(drawn_image)
 
 
-def confirm_video(video_name):
+def evaluate_video(video_path):
     motion_frames = []
-
-    video_frames = glob.glob('./' + video_name + '/*.png')[:5]
+    video_frames = glob.glob(video_path + '/*.png')[:5]
     for image_name in video_frames:
-        show_image(image_name)
-
+        frame_n = image_name.split('/')[-1].split('.png')[0]
+        if show_image(image_name, frame_n) == 'mark':
+            motion_frames.append(frame_n)
     return motion_frames
 
+
+def confirm_many_videos(path_prefix='data/prediction_videos_final_'):
+    with open('./movies_sorted_by_length.csv', 'r') as f:
+        reader = csv.reader(f)
+        file_names = [path_prefix + fn[0] for fn in list(reader)]
+    for vid in file_names[500:505]:
+        print(evaluate_video(vid))
+
+
 if __name__ == '__main__':
-    confirm_video('79_2')
+    confirm_many_videos('data/prediction_videos_final_')
