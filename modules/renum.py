51045b413a0c9b2b6013e09f669ca80495f3ab45
drschwenk <schwgit2@gmail.com>
2016-04-14 17:43:11 -0700

    renaming script working

--- modules/renum.py
+++ modules/renum.py
@@ -12,9 +12,10 @@ def get_name_parts(dir_name):
 def renumber_dir(cat_path, new_name, old_name, child_idx):
     new_name = str(new_name).zfill(3) + '_' + child_idx
     old_name = old_name + '_' + child_idx
-    old_path = cat_path + old_name
-    new_path = cat_path + new_name
-    print(old_path, new_path)
+    old_path = cat_path + '/' + old_name
+    new_path = cat_path + '/' + new_name
+    print('moving ', old_path, new_path)
+    os.rename(old_path, new_path)
 
 
 def renumber_category(cat_path):
@@ -22,23 +23,16 @@ def renumber_category(cat_path):
     path_to_first, first_name = first_dir.rsplit('/', maxsplit=1)
     fvn, fcn = get_name_parts(first_name)
     if '001' == fvn:
+        print( cat_path, ' good')
         return
-    print('here')
 
     for img_dir in glob.glob(cat_path + '/*')[1:]:
         path_to_dir, dir_name = img_dir.rsplit('/', maxsplit=1)
         pvn, mvn = get_name_parts(dir_name)
         new_parent_idx = int(pvn) - int(fvn)
-        # print(new_parent_idx)
         renumber_dir(cat_path, new_parent_idx, pvn, mvn)
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    # cat_dir_path = './img/falling-diving/'
-    cat_dir_path = './img/swinging-rope/'
-    renumber_category(cat_dir_path)
+    for tt_split in glob.glob('./data/*'):
+        for cat_dir_path in glob.glob(tt_split + '/*'):
+            renumber_category(cat_dir_path)
